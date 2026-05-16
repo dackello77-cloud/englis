b41dadf24d1f4a27dc49dc2424864bc00af3054c
dackello77-cloud <dackello77@gmail.com>
2026-05-16 16:11:37 +0200

    Add files via upload

---
+++ words.js
@@ -0,0 +1,17 @@
+window.WORDS_DATA = [
+  {
+    en: "garden",
+    read: "ˈɡärdən",
+    sr: "bašta"
+  },
+  {
+    en: "driver",
+    read: "ˈdraɪvər",
+    sr: "vozač"
+  },
+  {
+    en: "company",
+    read: "ˈkʌmpəni",
+    sr: "kompanija"
+  }
+];
